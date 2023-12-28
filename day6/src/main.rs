3e17fcf60abdbdf32d2fb414d4674d2f3c51fcec
Rik Voorhaar <rik.voorhaar@gmail.com>
2023-12-28 13:34:11 +0100

    day6-2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -10,12 +10,12 @@ fn main() -> Result<()> {
 
     let re_number = Regex::new(r"(\d+)").unwrap();
     let mut lines = reader.lines();
-    let times: Vec<u32>;
-    let distances: Vec<u32>;
+    let times: Vec<u64>;
+    let distances: Vec<u64>;
     if let Some(line) = lines.next() {
         times = re_number
             .find_iter(&line?)
-            .map(|x| x.as_str().parse::<u32>().unwrap())
+            .map(|x| x.as_str().parse::<u64>().unwrap())
             .collect();
     } else {
         return Err(anyhow!("Empty line"));
@@ -23,7 +23,7 @@ fn main() -> Result<()> {
     if let Some(line) = lines.next() {
         distances = re_number
             .find_iter(&line?)
-            .map(|x| x.as_str().parse::<u32>().unwrap())
+            .map(|x| x.as_str().parse::<u64>().unwrap())
             .collect();
     } else {
         return Err(anyhow!("Empty line"));
@@ -32,14 +32,15 @@ fn main() -> Result<()> {
     println!("{:?}", times);
     println!("{:?}", distances);
 
-    let output: u32 = times
+    let output: u64 = times
         .iter()
         .zip(distances.iter())
         .map(|(race_time, distance)| compute_c_range(*race_time, *distance))
         .product();
     println!("Output: {}", output);
 
-    
+    println!("{}", compute_c_range(71530, 940200));
+    println!("{}", compute_c_range(47707566,282107911471062));
 
     Ok(())
 }
@@ -50,20 +51,20 @@ fn main() -> Result<()> {
 // According to wolfram alpha we get
 // 1/2 (r-sqrt(r^2-4d) < c < 1/2 (sqrt(r^2-4d) + r)
 
-fn compute_c_range(race_time: u32, distance: u32) -> u32 {
-    let r = race_time as f32;
-    let d = distance as f32;
+fn compute_c_range(race_time: u64, distance: u64) -> u64 {
+    let r = race_time as f64;
+    let d = distance as f64;
     let discriminant = (r.powi(2) - 4.0 * d).sqrt();
     let mut lower = (r - discriminant) / 2.0;
     if lower.ceil() == lower {
         lower += 1.0
     }
-    let lower_int = lower.ceil() as u32;
+    let lower_int = lower.ceil() as u64;
     let mut upper = (r + discriminant) / 2.0;
     if upper == upper.floor() {
         upper -= 1.0
     }
-    let upper_int = upper.floor() as u32;
+    let upper_int = upper.floor() as u64;
     println!("lower: {}, upper: {}", lower_int, upper_int);
 
     upper_int - lower_int + 1
