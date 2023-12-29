06cf7d0162d443a9da98a6aade72c24e80db0757
Rik Voorhaar <rik.voorhaar@gmail.com>
2023-12-29 22:41:37 +0100

    day9-2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -26,32 +26,26 @@ fn diff(input: Vec<i32>) -> (Vec<i32>, bool) {
 
 fn make_predict(input: &[i32]) -> i32 {
     let mut all_zero = input.iter().filter(|&x| *x != 0).count() == 0;
-    let mut sum = *input.iter().last().unwrap();
     // let mut tower: Vec<Vec<i32>> = vec![input.to_vec()];
     let mut val = input.to_vec();
+    let mut first_vals = vec![input[0]];
     while !all_zero {
         let (new_diff, new_all_zero) = diff(val);
+        first_vals.push(new_diff[0]);
         val = new_diff;
-        sum += val.iter().last().unwrap();
         all_zero = new_all_zero;
     }
-
-    sum
+    // println!("{:?}", first_vals);
+    first_vals.iter().rev().fold(0, |acc, x| x - acc)
 }
 
 fn main() -> Result<()> {
     let file = File::open("day9/src/input.txt")?;
     let reader = BufReader::new(file);
     let input_vecs: Vec<Vec<i32>> = reader.lines().map(|l| extract_line(&l.unwrap())).collect();
-    println!("{:?}", input_vecs);
 
-    // let diff_tower = make_diff_tower(&input_vecs[0]);
-    println!("Predict {}", make_predict(&input_vecs[0]));
     let sum = input_vecs.iter().map(|x| make_predict(x)).sum::<i32>();
     println!("Sum {}", sum);
-
-    
-
 
     Ok(())
 }
