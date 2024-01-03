c58de4667079882ee76b63df149bbbb8cc52f1cb
Rik Voorhaar <rik.voorhaar@gmail.com>
2024-01-03 23:11:53 +0100

    day15-2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -15,18 +15,6 @@ struct Lens {
     focal_length: u8,
 }
 
-#[derive(Eq, PartialEq, Hash, Clone, Debug)]
-struct MapEntry {
-    counter: usize,
-    focal_length: u8,
-}
-
-#[derive(Clone, Debug)]
-struct Box {
-    insertion_counter: usize,
-    lenses: HashMap<String, MapEntry>,
-}
-
 struct Boxes {
     boxes: [Box; 256],
 }
@@ -38,17 +26,27 @@ impl Boxes {
                 label: label.to_string(),
                 focal_length: focal_length.parse::<u8>().unwrap(),
             };
-            println!("Inserting into box {} lens {:?}", hash, lens);
             self.boxes[hash].add_lens(lens);
             return;
         }
 
         if let Some((label, _)) = input.split_once('-') {
             let hash = hash_string(label);
-            println!("Removing from box {} lens {}", hash, label);
             self.boxes[hash].remove_lens(label)
         }
     }
+}
+
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+struct MapEntry {
+    counter: usize,
+    focal_length: u8,
+}
+
+#[derive(Clone, Debug)]
+struct Box {
+    insertion_counter: usize,
+    lenses: HashMap<String, MapEntry>,
 }
 
 impl Box {
@@ -78,10 +76,20 @@ impl Box {
     fn remove_lens(&mut self, label: &str) {
         self.lenses.remove(label);
     }
+
+    fn focusing_power(&self) -> usize {
+        let mut entries: Vec<&MapEntry> = self.lenses.values().collect();
+        entries.sort_by(|a, b| a.counter.cmp(&b.counter));
+        entries
+            .iter()
+            .enumerate()
+            .map(|(i, entry)| (i + 1) * (entry.focal_length as usize))
+            .sum()
+    }
 }
 
 fn main() -> Result<()> {
-    let input = std::fs::read_to_string("day15/src/example.txt")?;
+    let input = std::fs::read_to_string("day15/src/input.txt")?;
     let mut boxes = Boxes {
         boxes: (0..256)
             .map(|_| Box::new())
@@ -92,6 +100,13 @@ fn main() -> Result<()> {
     for piece in input.split(',') {
         boxes.insert(piece);
     }
+    let sum: usize = boxes
+        .boxes
+        .iter()
+        .enumerate()
+        .map(|(i, lens_box)| (i + 1) * lens_box.focusing_power())
+        .sum();
+    println!("Sum is {}", sum);
 
     Ok(())
 }
